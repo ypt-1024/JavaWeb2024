75551f909608f52493629ac77a5726847dffa6f7
yupengtao <javaer@111.com>
2024-02-27 17:34:40 +0800

    第一个servlet文件

---
+++ src/main/java/com/this0/servlet/Hello/TestServlet.java
@@ -0,0 +1,23 @@
+package com.this0.servlet.Hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String fname = req.getParameter("fname");
+        Integer price = Integer.valueOf(req.getParameter("price"));
+        Integer fcount = Integer.valueOf(req.getParameter("fcount"));
+        String remark = req.getParameter("remark");
+
+        System.out.println("fname=" + fname);
+        System.out.println("price=" + price);
+        System.out.println("fcount=" + fcount);
+        System.out.println("remark=" + remark);
+    }
+}
