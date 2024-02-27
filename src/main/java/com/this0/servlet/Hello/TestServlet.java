ec427a05adfa31d1b2e9e5e0ecbd312af99ff566
yupengtao <javaer@111.com>
2024-02-27 18:22:11 +0800

    解释第一个servlet文件 使用@WebServlet替代xml配置

--- src/main/java/com/this0/servlet/Hello/TestServlet.java
+++ src/main/java/com/this0/servlet/Hello/TestServlet.java
@@ -1,11 +1,14 @@
 package com.this0.servlet.Hello;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "TestServlet", value = "/fruit")
 public class TestServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
