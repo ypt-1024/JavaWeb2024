468b13d958f90161454b59fbad5f6d646b42b9ba
yupengtao <ypt@this0.com>
2024-04-02 23:42:52 +0800

    demo04——事件的绑定&常见事件修饰符

--- src/main/java/com/this0/vite/blog-vue/src/my.js
+++ src/main/java/com/this0/vite/blog-vue/src/my.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 
 import Demo03 from './components/demo03.vue'
+import Demo04 from './components/demo04.vue'
 
 createApp(Demo03).mount('#demo03')
+createApp(Demo04).mount('#demo04')
