f8d3dfa45d70c9ac7be0a723a9861fbbd58e2311
yupengtao <ypt@this0.com>
2024-04-02 22:47:04 +0800

    demo03——文本渲染

--- src/main/java/com/this0/vite/blog-vue/src/components/demo03.vue
+++ src/main/java/com/this0/vite/blog-vue/src/components/demo03.vue
@@ -1,36 +1,25 @@
 <script setup type="module">
 let msg = 'hello vue3'
 let getMsg = () => {
-  return 'hello vue3 message'
+  return msg
 }
 let age = 19
 let bee = '蜜 蜂'
-// 购物车
-const carts = [
-  { name: '可乐', price: 3, number: 10 },
-  { name: '薯片', price: 6, number: 8 },
-]
-//计算购物车总金额
-function compute() {
-  let count = 0
-  for (let index in carts) {
-    count += carts[index].price * carts[index].number
-  }
-  return count
-}
+let redMsg = "<font color='red'>msg</font>"
+let greenMsg = `<font color=\'green\'>${msg}</font>`
 </script>
 
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    msg的值为: {{ msg }} <br />
-    getMsg返回的值为:{{ getMsg() }} <br />
-    是否成年: {{ age >= 18 ? 'true' : 'false' }} <br />
-    反转: {{ bee.split(' ').reverse().join('-') }} <br />
-    购物车总金额: {{ compute() }} <br />
-    购物车总金额:
-    {{ carts[0].price * carts[0].number + carts[1].price * carts[1].number }}
-    <br />
+    <span v-text="msg"></span> <br />
+    <span v-text="redMsg"></span> <br />
+    <span v-text="getMsg()"></span> <br />
+    <span v-text="age > 18 ? '成年' : '未成年'"></span> <br />
+    <span v-text="bee.split(' ').reverse().join('-')"></span> <br />
+    <span v-html="msg"></span> <br />
+    <span v-html="redMsg"></span> <br />
+    <span v-html="greenMsg"></span> <br />
+    <span v-html="`<font color='green'>${msg}</font>`"></span> <br />
   </div>
 </template>
 
