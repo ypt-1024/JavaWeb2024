d7b3ae409a4bee1b0a4645bd5f407faef35157e1
yupengtao <ypt@this0.com>
2024-04-02 22:59:21 +0800

    demo03——属性渲染

--- src/main/java/com/this0/vite/blog-vue/src/components/demo03.vue
+++ src/main/java/com/this0/vite/blog-vue/src/components/demo03.vue
@@ -1,25 +1,18 @@
 <script setup type="module">
-let msg = 'hello vue3'
-let getMsg = () => {
-  return msg
+const data = {
+  name: 'this0',
+  url: 'http://www.this0.com',
+  logo: 'https://blog-resources.this0.com/base/logo.png',
 }
-let age = 19
-let bee = '蜜 蜂'
-let redMsg = "<font color='red'>msg</font>"
-let greenMsg = `<font color=\'green\'>${msg}</font>`
 </script>
 
 <template>
   <div>
-    <span v-text="msg"></span> <br />
-    <span v-text="redMsg"></span> <br />
-    <span v-text="getMsg()"></span> <br />
-    <span v-text="age > 18 ? '成年' : '未成年'"></span> <br />
-    <span v-text="bee.split(' ').reverse().join('-')"></span> <br />
-    <span v-html="msg"></span> <br />
-    <span v-html="redMsg"></span> <br />
-    <span v-html="greenMsg"></span> <br />
-    <span v-html="`<font color='green'>${msg}</font>`"></span> <br />
+    <a v-bind:href="data.url" target="_self">
+      <img :src="data.logo" :title="data.name" />
+      <br />
+      <input type="button" :value="`点击访问${data.name}`" />
+    </a>
   </div>
 </template>
 
