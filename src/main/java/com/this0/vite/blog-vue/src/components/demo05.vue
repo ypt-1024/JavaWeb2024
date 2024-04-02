175fd985e223dc19b0e92b9bea6abf1a19a3c2a4
yupengtao <ypt@this0.com>
2024-04-02 23:50:35 +0800

    demo05——没使用响应式的情况演示

---
+++ src/main/java/com/this0/vite/blog-vue/src/components/demo05.vue
@@ -0,0 +1,21 @@
+<script type="module" setup>
+    let counter = 0;
+    function show(){
+        alert(counter);
+    }
+</script>
+
+<template>
+  <div>
+    <button @click="counter--">-</button> 
+    {{ counter }} 
+    <button @click="counter++">+</button>
+    <hr>
+    <!-- 此案例,我们发现counter值,会改变,但是页面不改变! 默认Vue3的数据是非响应式的!-->
+    <button @click="show()">显示counter值</button>
+   </div>
+</template> 
+
+<style scoped>
+
+</style>
--- src/main/java/com/this0/vite/blog-vue/src/my.js
+++ src/main/java/com/this0/vite/blog-vue/src/my.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 
 import Demo03 from './components/demo03.vue'
 import Demo04 from './components/demo04.vue'
+import Demo05 from './components/demo05.vue'
 
 createApp(Demo03).mount('#demo03')
 createApp(Demo04).mount('#demo04')
+createApp(Demo05).mount('#demo05')
