ec7caa03c914d996f263e0c2b50073ed2a1a036c
yupengtao <ypt@this0.com>
2024-04-03 02:32:00 +0800

    demo06——条件渲染

---
+++ src/main/java/com/this0/vite/blog-vue/src/components/demo06.vue
@@ -0,0 +1,15 @@
+<script type="module" setup>
+import { ref } from 'vue'
+let awesome = ref(true)
+</script>
+
+<template>
+  <div>
+    <h1 id="ha" v-show="awesome">Vue is awesome!</h1>
+    <h1 id="hb" v-if="awesome">Vue is awesome!</h1>
+    <h1 id="hc" v-else>Oh no 😢</h1>
+    <button @click="awesome = !awesome">Toggle</button>
+  </div>
+</template>
+
+<style scoped></style>
--- src/main/java/com/this0/vite/blog-vue/src/my.js
+++ src/main/java/com/this0/vite/blog-vue/src/my.js
@@ -3,7 +3,9 @@ import { createApp } from 'vue'
 import Demo03 from './components/demo03.vue'
 import Demo04 from './components/demo04.vue'
 import Demo05 from './components/demo05.vue'
+import Demo06 from './components/demo06.vue'
 
 createApp(Demo03).mount('#demo03')
 createApp(Demo04).mount('#demo04')
 createApp(Demo05).mount('#demo05')
+createApp(Demo06).mount('#demo06')
