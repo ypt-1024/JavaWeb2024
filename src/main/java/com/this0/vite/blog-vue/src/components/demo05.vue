fccf7fff4b35469b8e1057394ca20ece8b9a65a9
yupengtao <ypt@this0.com>
2024-04-03 02:14:15 +0800

    demo05——ref实现响应式

--- src/main/java/com/this0/vite/blog-vue/src/components/demo05.vue
+++ src/main/java/com/this0/vite/blog-vue/src/components/demo05.vue
@@ -1,21 +1,29 @@
 <script type="module" setup>
-    let counter = 0;
-    function show(){
-        alert(counter);
-    }
+/* 从vue中引入ref方法 */
+import { ref } from 'vue'
+let counter = ref(0)
+function show() {
+  alert(counter.value)
+}
+/* 函数中要操作ref处理过的数据,需要通过.value形式 */
+let decr = () => {
+  counter.value--
+}
+let incr = () => {
+  counter.value++
+}
 </script>
 
 <template>
   <div>
-    <button @click="counter--">-</button> 
-    {{ counter }} 
+    <button @click="counter--">-</button>
+    <button @click="decr()">-</button>
+    {{ counter }}
     <button @click="counter++">+</button>
-    <hr>
-    <!-- 此案例,我们发现counter值,会改变,但是页面不改变! 默认Vue3的数据是非响应式的!-->
+    <button @click="incr()">+</button>
+    <hr />
     <button @click="show()">显示counter值</button>
-   </div>
-</template> 
+  </div>
+</template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
