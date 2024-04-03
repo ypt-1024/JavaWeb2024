e94be968d968efbd0ebc0cb57dd893ff3b6ed80f
yupengtao <ypt@this0.com>
2024-04-03 14:11:10 +0800

    demo08——双向数据绑定

---
+++ src/main/java/com/this0/vite/blog-vue/src/components/demo08.vue
@@ -0,0 +1,53 @@
+<script type="module" setup>
+
+//引入模块
+import { reactive,ref} from 'vue'
+let hbs = ref([]); //装爱好的值
+let user = reactive({username:null,password:null,introduce:null,pro:null})
+function login(){
+  alert(hbs.value);
+  alert(JSON.stringify(user));
+}
+function clearx(){
+  //user = {};// 这中写法会将数据变成非响应的,应该是user.username=""
+  user.username=''
+  user.password=''
+  user.introduce=''
+  user.pro=''
+  hbs.value.splice(0,hbs.value.length);;
+}
+</script>
+
+<template>
+  <div>
+    账号： <input type="text" placeholder="请输入账号！" v-model="user.username"> <br>
+    密码： <input type="text" placeholder="请输入账号！" v-model="user.password"> <br>
+    爱好：
+    吃 <input type="checkbox" name="hbs" v-model="hbs" value="吃">
+    喝 <input type="checkbox" name="hbs" v-model="hbs" value="喝">
+    玩 <input type="checkbox" name="hbs" v-model="hbs" value="玩">
+    乐 <input type="checkbox" name="hbs" v-model="hbs" value="乐">
+    <br>
+    简介:<textarea v-model="user.introduce"></textarea>
+    <br>
+    籍贯:
+    <select v-model="user.pro">
+      <option value="1">黑</option>
+      <option value="2">吉</option>
+      <option value="3">辽</option>
+      <option value="4">京</option>
+      <option value="5">津</option>
+      <option value="6">冀</option>
+    </select>
+    <br>
+    <button @click="login()">登录</button>
+    <button @click="clearx()">重置</button>
+    <hr>
+    显示爱好:{{ hbs }}
+    <hr>
+    显示用户信息:{{ user }}
+  </div>
+</template>
+
+<style scoped>
+</style>
--- src/main/java/com/this0/vite/blog-vue/src/my.js
+++ src/main/java/com/this0/vite/blog-vue/src/my.js
@@ -5,9 +5,11 @@ import Demo04 from './components/demo04.vue'
 import Demo05 from './components/demo05.vue'
 import Demo06 from './components/demo06.vue'
 import Demo07 from './components/demo07.vue'
+import Demo08 from './components/demo08.vue'
 
 createApp(Demo03).mount('#demo03')
 createApp(Demo04).mount('#demo04')
 createApp(Demo05).mount('#demo05')
 createApp(Demo06).mount('#demo06')
 createApp(Demo07).mount('#demo07')
+createApp(Demo08).mount('#demo08')
