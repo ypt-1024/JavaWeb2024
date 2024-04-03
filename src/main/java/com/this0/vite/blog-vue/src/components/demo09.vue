6232b8602b911639458b2535579ce6013a7212a9
yupengtao <ypt@this0.com>
2024-04-03 18:52:40 +0800

    demo09——使用计算属性之前

---
+++ src/main/java/com/this0/vite/blog-vue/src/components/demo09.vue
@@ -0,0 +1,21 @@
+<script type="module" setup>
+//引入模块
+import { reactive, computed } from 'vue'
+const author = reactive({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Advanced Guide',
+    'Vue 3 - Basic Guide',
+    'Vue 4 - The Mystery',
+  ],
+})
+</script>
+
+<template>
+  <div>
+    <p>{{ author.name }} Has published books?:</p>
+    <span>{{ author.books.length > 0 ? 'Yes' : 'No' }}</span>
+  </div>
+</template>
+
+<style scoped></style>
--- src/main/java/com/this0/vite/blog-vue/src/my.js
+++ src/main/java/com/this0/vite/blog-vue/src/my.js
@@ -6,6 +6,7 @@ import Demo05 from './components/demo05.vue'
 import Demo06 from './components/demo06.vue'
 import Demo07 from './components/demo07.vue'
 import Demo08 from './components/demo08.vue'
+import Demo09 from './components/demo09.vue'
 
 createApp(Demo03).mount('#demo03')
 createApp(Demo04).mount('#demo04')
@@ -13,3 +14,4 @@ createApp(Demo05).mount('#demo05')
 createApp(Demo06).mount('#demo06')
 createApp(Demo07).mount('#demo07')
 createApp(Demo08).mount('#demo08')
+createApp(Demo09).mount('#demo09')
