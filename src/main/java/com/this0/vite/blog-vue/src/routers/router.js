95e1414679312c1f9b85505b1893c94ab6d5d18c
yupengtao <ypt@this0.com>
2024-04-11 18:21:50 +0800

    路由守卫

--- src/main/java/com/this0/vite/blog-vue/src/routers/router.js
+++ src/main/java/com/this0/vite/blog-vue/src/routers/router.js
@@ -31,5 +31,30 @@ const router = createRouter({
 
 })
 
+//全局前置路由守卫
+router.beforeEach( (to,from,next) => {
+
+    //to 是目标地包装对象  .path属性可以获取地址
+    //from 是来源地包装对象 .path属性可以获取地址
+
+    //next() 放行,直接到达目标组件
+    //next('/地址')可以转发到其他地址,到达目标组件前会再次经过前置路由守卫
+
+
+    //需要判断，注意避免无限重定向
+    if(to.path == '/showDetail'){
+        next()
+    }else{
+        next('/showDetail')
+    }
+
+} )
+
+//全局后置路由守卫
+router.afterEach((to, from) => {
+    console.log(`路由从 ${from.path} 到 ${to.path}`);
+});
+
+
 // 对外暴露路由对象
 export default router;
